5ed2d9892bd087719494a74bdbb90ca8edb587bf
Jo <johj703@nate.com>
2024-09-24 16:31:39 +0900

    fix: 타입스크립트에서 bollean 타입으로 타입을 추론할 때는 어노테이션이 필요하다.

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
+let numbers = [-10, -5, 10];
+let numberAboveZero: boolean | number = false; // boolean 타입으로 타입 추론 -> 타입 어노테이션 필요 boolean | number
 
-const sum: string = add(1, 2);
-console.log(sum);
+for (let i = 0; i < colors.length; i++) {
+  if (numbers[i] > 0) numberAboveZero = numbers[i]; // error
+}
